c8094e810469305590ff8f6182e7a8619e70b37d
InvinciblePyro <samrexspivey@gmail.com>
2025-05-22 21:07:55 -0700

    tweaked the drag var

--- PlatformImprovement-master/src/Scenes/Platformer.js
+++ PlatformImprovement-master/src/Scenes/Platformer.js
@@ -6,11 +6,11 @@ class Platformer extends Phaser.Scene {
     init() {
         // variables and settings
         this.ACCELERATION = 400;
-        this.DRAG = 500;    // DRAG < ACCELERATION = icy slide
+        this.DRAG = 2000;    // DRAG < ACCELERATION = icy slide
         this.physics.world.gravity.y = 1500;
         this.JUMP_VELOCITY = -600;
         this.PARTICLE_VELOCITY = 50;
-        console.log(window.innerHeight);
+
         let desiredScale = 2;
         if(window.innerHeight<=400){desiredScale=1}
         else if (window.innerHeight<=600){desiredScale=1.5}
